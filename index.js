1453a8a8f79e5e3ddf8b29eed7182757e0685f42
DarkAsfu <ashraful.islam.asfu@gmail.com>
2023-05-03 16:45:39 +0600

    Chefs json file create

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+const chefs = require('./data/chefs.json');
 
+app.use(cors());
 app.get('/', (req, res) =>{
     res.send('The hungry cat coming !!!....')
 })
-
+app.get('/chefs', (req, res) =>{
+    res.send(chefs)
+})
 app.listen(port, () =>{
     console.log(`The hungry cat api running on port: ${port}`)
 })
